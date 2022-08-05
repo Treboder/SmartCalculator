ee4dcb116f37555b23178727afb141aa2c77f403
Treboder <robert.kohn@gmx.net>
2022-08-05 22:38:58 +0200

    Stage 8/8: Very big (with stolen code https://github.com/marckoch)

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -1,14 +1,14 @@
 
+import java.math.BigInteger
 import java.util.*
 import kotlin.collections.ArrayDeque
-import kotlin.math.pow
 
 fun main() {
     Calculator().go()
 }
 
 class Calculator {
-    private val variables = emptyMap<String, Int>().toMutableMap()
+    private val variables = emptyMap<String, BigInteger>().toMutableMap()
 
     fun go() {
         while (true) {
@@ -49,22 +49,22 @@ class Calculator {
         }
     }
 
-    private fun evaluateRPN(queue: ArrayDeque<Token>): Int {
+    private fun evaluateRPN(queue: ArrayDeque<Token>): BigInteger {
         val stack = Stack<Token>()
 
         for (token in queue) {
             if (token.isNumber()) {
                 stack.push(token)
             } else if (token.isOperator()) {
-                val y = stack.pop().toInt() // order is reversed! the R in RPN!
-                val x = stack.pop().toInt() // order is reversed! the R in RPN!
+                val y = stack.pop().toBigInteger() // order is reversed! the R in RPN!
+                val x = stack.pop().toBigInteger() // order is reversed! the R in RPN!
 
                 val result = when (token.toString()) {
                     "+" -> x + y
                     "-" -> x - y
                     "*" -> x * y
                     "/" -> x / y
-                    "^" -> x.toDouble().pow(y).toInt()
+                    "^" -> x.pow(y.toInt()).toBigDecimal().toBigInteger()
                     else -> IllegalArgumentException("Unknown operator: $token")
                 }
 
@@ -74,7 +74,7 @@ class Calculator {
 
         assert(stack.size == 1) { "stack should only have one element at the end of RPN evaluation! but has: $stack" }
 
-        return stack.pop().toInt()
+        return stack.pop().toBigInteger()
     }
 
     private fun tokenize(line: String): MutableList<String> {
@@ -176,7 +176,7 @@ class Calculator {
             throw InvalidIdentifierException()
         }
 
-        variables[variableName] = value.toIntOrNull() ?: variables[value] ?: throw UnknownVariableException()
+        variables[variableName] = value.toBigIntegerOrNull() ?: variables[value] ?: throw UnknownVariableException()
     }
 
     private fun isValidVariableName(name: String): Boolean {
@@ -184,7 +184,7 @@ class Calculator {
     }
 
     private fun isNumber(name: String): Boolean {
-        return name.toIntOrNull() != null
+        return name.toBigIntegerOrNull() != null
     }
 }
 
@@ -194,7 +194,7 @@ class InvalidIdentifierException : Exception("Invalid identifier")
 
 data class Token(private val token: String) {
     fun isNumber(): Boolean {
-        return token.toIntOrNull() != null
+        return token.toBigIntegerOrNull() != null
     }
 
     fun isOperator(): Boolean {
@@ -209,8 +209,8 @@ data class Token(private val token: String) {
         return token == ")"
     }
 
-    fun toInt(): Int {
-        return token.toInt()
+    fun toBigInteger(): BigInteger {
+        return token.toBigInteger()
     }
 
     // used for operator precedence
